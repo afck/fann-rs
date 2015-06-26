4c05dabb79ff694475ac1a48c4338a478c1ca486
Andreas Fackler <AndreasFackler@gmx.de>
2015-06-26 14:44:03 +0300

    Implement basic functionality needed to run the tutorial.

---
+++ src/error.rs
@@ -0,0 +1,127 @@
+use fann_sys::{fann_error, fann_get_errno, fann_get_errstr};
+use fann_sys::fann_errno_enum::*;
+use self::FannErrorType::*;
+use std::error::Error;
+use std::fmt;
+use std::ffi::CStr;
+
+#[derive(Copy, Clone, Debug)]
+pub enum FannErrorType {
+    /// Unable to open configuration file for reading
+    CantOpenConfigR,
+    /// Unable to open configuration file for writing
+    CantOpenConfigW,
+    /// Wrong version of configuration file
+    WrongConfigVersion,
+    /// Error reading info from configuration file
+    CantReadConfig,
+    /// Error reading neuron info from configuration file
+    CantReadNeuron,
+    /// Error reading connections from configuration file
+    CantReadConnections,
+    /// Number of connections not equal to the number expected
+    WrongNumConnections,
+    /// Unable to open train data file for writing
+    CantOpenTdW,
+    /// Unable to open train data file for reading
+    CantOpenTdR,
+    /// Error reading training data from file
+    CantReadTd,
+    /// Unable to allocate memory
+    CantAllocateMem,
+    /// Unable to train with the selected activation function
+    CantTrainActivation,
+    /// Unable to use the selected activation function
+    CantUseActivation,
+    /// Irreconcilable differences between two `fann_train_data` structures
+    TrainDataMismatch,
+    /// Unable to use the selected training algorithm
+    CantUseTrainAlg,
+    /// Trying to take subset which is not within the training set
+    TrainDataSubset,
+    /// Index is out of bound
+    IndexOutOfBound,
+    /// Scaling parameters not present
+    ScaleNotPresent,
+}
+
+impl fmt::Display for FannErrorType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        fmt::Display::fmt(match *self {
+            CantOpenConfigR     => "Unable to open configuration file for reading",
+            CantOpenConfigW     => "Unable to open configuration file for writing",
+            WrongConfigVersion  => "Wrong version of configuration file",
+            CantReadConfig      => "Error reading info from configuration file",
+            CantReadNeuron      => "Error reading neuron info from configuration file",
+            CantReadConnections => "Error reading connections from configuration file",
+            WrongNumConnections => "Number of connections not equal to the number expected",
+            CantOpenTdW         => "Unable to open train data file for writing",
+            CantOpenTdR         => "Unable to open train data file for reading",
+            CantReadTd          => "Error reading training data from file",
+            CantAllocateMem     => "Unable to allocate memory",
+            CantTrainActivation => "Unable to train with the selected activation function",
+            CantUseActivation   => "Unable to use the selected activation function",
+            TrainDataMismatch   => "Irreconcilable differences between two Fann objects",
+            CantUseTrainAlg     => "Unable to use the selected training algorithm",
+            TrainDataSubset     => "Trying to take subset which is not within the training set",
+            IndexOutOfBound     => "Index is out of bound",
+            ScaleNotPresent     => "Scaling parameters not present",
+        }, f)
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct FannError {
+    pub error_type: FannErrorType,
+    pub error_str: String,
+}
+
+impl fmt::Display for FannError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        try!(self.error_type.fmt(f));
+        try!(": ".fmt(f));
+        self.error_str.fmt(f)
+    }
+}
+
+impl Error for FannError {
+    fn description(&self) -> &str {
+        &self.error_str[..]
+    }
+}
+
+impl FannError {
+    /// Returns an `Err` if the previous operation on `errdat` has resulted in an error, otherwise
+    /// `Ok(())`.
+    pub unsafe fn check_no_error(errdat: *mut fann_error) -> Result<(), FannError> {
+        let error_type = match fann_get_errno(errdat) {
+            FANN_E_NO_ERROR              => return Ok(()),
+            FANN_E_CANT_OPEN_CONFIG_R    => CantOpenConfigR,
+            FANN_E_CANT_OPEN_CONFIG_W    => CantOpenConfigW,
+            FANN_E_WRONG_CONFIG_VERSION  => WrongConfigVersion,
+            FANN_E_CANT_READ_CONFIG      => CantReadConfig,
+            FANN_E_CANT_READ_NEURON      => CantReadNeuron,
+            FANN_E_CANT_READ_CONNECTIONS => CantReadConnections,
+            FANN_E_WRONG_NUM_CONNECTIONS => WrongNumConnections,
+            FANN_E_CANT_OPEN_TD_W        => CantOpenTdW,
+            FANN_E_CANT_OPEN_TD_R        => CantOpenTdR,
+            FANN_E_CANT_READ_TD          => CantReadTd,
+            FANN_E_CANT_ALLOCATE_MEM     => CantAllocateMem,
+            FANN_E_CANT_TRAIN_ACTIVATION => CantTrainActivation,
+            FANN_E_CANT_USE_ACTIVATION   => CantUseActivation,
+            FANN_E_TRAIN_DATA_MISMATCH   => TrainDataMismatch,
+            FANN_E_CANT_USE_TRAIN_ALG    => CantUseTrainAlg,
+            FANN_E_TRAIN_DATA_SUBSET     => TrainDataSubset,
+            FANN_E_INDEX_OUT_OF_BOUND    => IndexOutOfBound,
+            FANN_E_SCALE_NOT_PRESENT     => ScaleNotPresent,
+        };
+        let errstr_bytes = CStr::from_ptr(fann_get_errstr(errdat)).to_bytes().to_vec();
+        let error_str = String::from_utf8(errstr_bytes)
+            .unwrap_or("Invalid UTF-8 in error string".to_string());
+        Err(FannError {
+            error_type: error_type,
+            error_str: error_str,
+        })
+    }
+}
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,442 @@
-#[test]
-fn it_works() {
+extern crate libc;
+extern crate fann_sys;
+
+use error::{FannError, FannErrorType};
+use fann_sys::fann_activationfunc_enum::*;
+use fann_sys::fann_type;
+use libc::{c_float, c_uint};
+use std::ffi::CString;
+use std::path::Path;
+use std::ptr::copy_nonoverlapping;
+
+pub mod error;
+
+#[derive(Copy, Clone)]
+pub enum ActivationFunc {
+    /// Linear activation function.
+    ///
+    /// * span: -inf < y < inf
+    ///
+    /// * y = x*s, d = 1*s
+    ///
+    /// * Can NOT be used in fixed point.
+    Linear,
+    /// Threshold activation function.
+    ///
+    /// * x < 0 -> y = 0, x >= 0 -> y = 1
+    ///
+    /// * Can NOT be used during training.
+    Threshold,
+    /// Threshold activation function.
+    ///
+    /// * x < 0 -> y = 0, x >= 0 -> y = 1
+    ///
+    /// * Can NOT be used during training.
+    ThresholdSymmetric,
+    /// Sigmoid activation function.
+    ///
+    /// * One of the most used activation functions.
+    ///
+    /// * span: 0 < y < 1
+    ///
+    /// * y = 1/(1 + exp(-2*s*x))
+    ///
+    /// * d = 2*s*y*(1 - y)
+    Sigmoid,
+    /// Stepwise linear approximation to sigmoid.
+    ///
+    /// * Faster than sigmoid but a bit less precise.
+    SigmoidStepwise,
+    /// Symmetric sigmoid activation function, aka. tanh.
+    ///
+    /// * One of the most used activation functions.
+    ///
+    /// * span: -1 < y < 1
+    ///
+    /// * y = tanh(s*x) = 2/(1 + exp(-2*s*x)) - 1
+    ///
+    /// * d = s*(1-(y*y))
+    SigmoidSymmetric,
+    /// Stepwise linear approximation to symmetric sigmoid.
+    ///
+    /// * Faster than symmetric sigmoid but a bit less precise.
+    SigmoidSymmetricStepwise,
+    /// Gaussian activation function.
+    ///
+    /// * 0 when x = -inf, 1 when x = 0 and 0 when x = inf
+    ///
+    /// * span: 0 < y < 1
+    ///
+    /// * y = exp(-x*s*x*s)
+    ///
+    /// * d = -2*x*s*y*s
+    Gaussian,
+    /// Symmetric gaussian activation function.
+    ///
+    /// * -1 when x = -inf, 1 when x = 0 and 0 when x = inf
+    ///
+    /// * span: -1 < y < 1
+    ///
+    /// * y = exp(-x*s*x*s)*2-1
+    ///
+    /// * d = -2*x*s*(y+1)*s
+    GaussianSymmetric,
+    /// Stepwise linear approximation to gaussian.
+    /// Faster than gaussian but a bit less precise.
+    /// NOT implemented yet.
+    GaussianStepwise,
+    /// Fast (sigmoid like) activation function defined by David Elliott
+    ///
+    /// * span: 0 < y < 1
+    ///
+    /// * y = ((x*s) / 2) / (1 + |x*s|) + 0.5
+    ///
+    /// * d = s*1/(2*(1+|x*s|)*(1+|x*s|))
+    Elliott,
+    /// Fast (symmetric sigmoid like) activation function defined by David Elliott
+    ///
+    /// * span: -1 < y < 1
+    ///
+    /// * y = (x*s) / (1 + |x*s|)
+    ///
+    /// * d = s*1/((1+|x*s|)*(1+|x*s|))
+    ElliottSymmetric,
+    /// Bounded linear activation function.
+    ///
+    /// * span: 0 <= y <= 1
+    ///
+    /// * y = x*s, d = 1*s
+    LinearPiece,
+    /// Bounded linear activation function.
+    ///
+    /// * span: -1 <= y <= 1
+    ///
+    /// * y = x*s, d = 1*s
+    LinearPieceSymmetric,
+    /// Periodical sine activation function.
+    ///
+    /// * span: -1 <= y <= 1
+    ///
+    /// * y = sin(x*s)
+    ///
+    /// * d = s*cos(x*s)
+    SinSymmetric,
+    /// Periodical cosine activation function.
+    ///
+    /// * span: -1 <= y <= 1
+    ///
+    /// * y = cos(x*s)
+    ///
+    /// * d = s*-sin(x*s)
+    CosSymmetric,
+    /// Periodical sine activation function.
+    ///
+    /// * span: 0 <= y <= 1
+    ///
+    /// * y = sin(x*s)/2+0.5
+    ///
+    /// * d = s*cos(x*s)/2
+    Sin,
+    /// Periodical cosine activation function.
+    ///
+    /// * span: 0 <= y <= 1
+    ///
+    /// * y = cos(x*s)/2+0.5
+    ///
+    /// * d = s*-sin(x*s)/2
+    Cos,
+}
+
+impl ActivationFunc {
+    fn to_fann_activationfunc_enum(&self) -> fann_sys::fann_activationfunc_enum {
+        match *self {
+            ActivationFunc::Linear                   => FANN_LINEAR,
+            ActivationFunc::Threshold                => FANN_THRESHOLD,
+            ActivationFunc::ThresholdSymmetric       => FANN_THRESHOLD_SYMMETRIC,
+            ActivationFunc::Sigmoid                  => FANN_SIGMOID,
+            ActivationFunc::SigmoidStepwise          => FANN_SIGMOID_STEPWISE,
+            ActivationFunc::SigmoidSymmetric         => FANN_SIGMOID_SYMMETRIC,
+            ActivationFunc::SigmoidSymmetricStepwise => FANN_SIGMOID_SYMMETRIC_STEPWISE,
+            ActivationFunc::Gaussian                 => FANN_GAUSSIAN,
+            ActivationFunc::GaussianSymmetric        => FANN_GAUSSIAN_SYMMETRIC,
+            ActivationFunc::GaussianStepwise         => FANN_GAUSSIAN_STEPWISE,
+            ActivationFunc::Elliott                  => FANN_ELLIOTT,
+            ActivationFunc::ElliottSymmetric         => FANN_ELLIOTT_SYMMETRIC,
+            ActivationFunc::LinearPiece              => FANN_LINEAR_PIECE,
+            ActivationFunc::LinearPieceSymmetric     => FANN_LINEAR_PIECE_SYMMETRIC,
+            ActivationFunc::SinSymmetric             => FANN_SIN_SYMMETRIC,
+            ActivationFunc::CosSymmetric             => FANN_COS_SYMMETRIC,
+            ActivationFunc::Sin                      => FANN_SIN,
+            ActivationFunc::Cos                      => FANN_COS,
+        }
+    }
+}
+
+pub struct TrainData {
+    raw: *mut fann_sys::fann_train_data,
+}
+
+impl TrainData {
+    /// Read a file that stores training data.
+    ///
+    /// The file must be formatted like:
+    ///
+    /// ```text
+    /// num_train_data num_input num_output
+    /// inputdata separated by space
+    /// outputdata separated by space
+    /// .
+    /// .
+    /// .
+    /// inputdata separated by space
+    /// outputdata separated by space
+    /// ```
+    pub fn from_file<P: AsRef<Path>>(path: P) -> Result<TrainData, FannError> {
+        let filename = match path.as_ref().to_str().map(|s| CString::new(s)) {
+            None => return Err(FannError {
+                                   error_type: FannErrorType::CantOpenTdR,
+                                   error_str: "File name contains invalid unicode characters"
+                                                  .to_string(),
+                               }),
+            Some(Err(e)) => return Err(FannError {
+                                       error_type: FannErrorType::CantOpenTdR,
+                                       error_str: format!(
+                                           "File name contains a nul byte at position {}",
+                                           e.nul_position()),
+                               }),
+            Some(Ok(cs)) => cs,
+        };
+        unsafe {
+            let raw = fann_sys::fann_read_train_from_file(filename.as_ptr());
+            if raw.is_null() {
+                return Err(FannError {
+                    error_type: FannErrorType::CantAllocateMem,
+                    error_str: "Unable to create a new TrainData object".to_string(),
+                });
+            }
+            try!(FannError::check_no_error(raw as *mut fann_sys::fann_error));
+            Ok(TrainData { raw: raw })
+        }
+    }
+}
+
+impl Drop for TrainData {
+    fn drop(&mut self) {
+        unsafe {
+            fann_sys::fann_destroy_train(self.raw);
+        }
+    }
+}
+
+pub struct Fann {
+    // We don't consider setting and clearing the error string and number a mutation, and every
+    // method should leave these fields cleared, either because it succeeded or because it read the
+    // fields and returned the corresponding error.
+    // We also don't consider writing the output data a mutation, as we don't provide access to it
+    // and copy it before returning it.
+    raw: *mut fann_sys::fann,
+}
+
+impl Fann {
+    /// Create a fully connected neural network.
+    ///
+    /// There will be a bias neuron in each layer except the output layer,
+    /// and this bias neuron will be connected to all neurons in the next layer.
+    /// When running the network, the bias nodes always emit 1.
+    ///
+    /// # Arguments
+    ///
+    /// * `layers` - Specifies the number of neurons in each layer, starting with the input and
+    ///              ending with the output layer.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// // Creating a network with 2 input neurons, 1 output neuron,
+    /// // and two hidden layers with 8 and 9 neurons.
+    /// let layers = [2, 8, 9, 1];
+    /// fann::Fann::new(&layers).unwrap();
+    /// ```
+    pub fn new(layers: &[c_uint]) -> Result<Fann, FannError> {
+        Fann::new_sparse(1.0, layers)
+    }
+
+    /// Create a neural network that is not necessarily fully connected.
+    ///
+    /// There will be a bias neuron in each layer except the output layer,
+    /// and this bias neuron will be connected to all neurons in the next layer.
+    /// When running the network, the bias nodes always emit 1.
+    ///
+    /// # Arguments
+    ///
+    /// * `connection_rate` - The share of pairs of neurons in consecutive layers that will be
+    ///                       connected.
+    /// * `layers`          - Specifies the number of neurons in each layer, starting with the input
+    ///                       and ending with the output layer.
+    pub fn new_sparse(connection_rate: c_float, layers: &[c_uint]) -> Result<Fann, FannError> {
+        unsafe {
+            let raw = fann_sys::fann_create_sparse_array(connection_rate,
+                                                         layers.len() as c_uint,
+                                                         layers.as_ptr());
+            if raw.is_null() {
+                return Err(FannError {
+                    error_type: FannErrorType::CantAllocateMem,
+                    error_str: "Unable to create a new Fann object".to_string(),
+                });
+            }
+            try!(FannError::check_no_error(raw as *mut fann_sys::fann_error));
+            Ok(Fann { raw: raw })
+        }
+    }
+
+    /// Return an `Err` if the size of the slice does not match the number of input neurons,
+    /// otherwise `Ok(())`.
+    fn check_input_size(&self, input: &[fann_type]) -> Result<(), FannError> {
+        let num_input = self.get_num_input() as usize;
+        if input.len() == num_input {
+            Ok(())
+        } else {
+            Err(FannError {
+                error_type: FannErrorType::IndexOutOfBound, // TODO: New error type?
+                error_str: format!("Input has length {}, but there are {} input neurons",
+                                   input.len(), num_input),
+            })
+        }
+    }
+
+    /// Return an `Err` if the size of the slice does not match the number of output neurons,
+    /// otherwise `Ok(())`.
+    fn check_output_size(&self, output: &[fann_type]) -> Result<(), FannError> {
+        let num_output = self.get_num_output() as usize;
+        if output.len() == num_output {
+            Ok(())
+        } else {
+            Err(FannError {
+                error_type: FannErrorType::IndexOutOfBound, // TODO: New error type?
+                error_str: format!("Output has length {}, but there are {} output neurons",
+                                   output.len(), num_output),
+            })
+        }
+    }
+
+    pub fn train(&mut self, input: &[fann_type], desired_output: &[fann_type])
+            -> Result<(), FannError> {
+        unsafe {
+            try!(self.check_input_size(input));
+            try!(self.check_output_size(desired_output));
+            fann_sys::fann_train(self.raw, input.as_ptr(), desired_output.as_ptr());
+            try!(FannError::check_no_error(self.raw as *mut fann_sys::fann_error));
+        }
+        Ok(())
+    }
+
+    /// Train the network on the given data set.
+    ///
+    /// # Parameters
+    ///
+    /// * `data`                   - The training data.
+    /// * `max_epochs`             - The maximum number of training epochs.
+    /// * `epochs_between_reports` - The number of epochs between printing a status report to
+    ///                              `stdout`, or `0` to print no reports.
+    /// * `desired_error`          - The desired maximum value of `get_mse` or `get_bit_fail`,
+    ///                              depending on which stop function was selected.
+    pub fn train_on_data(&mut self,
+                         data: &TrainData,
+                         max_epochs: c_uint,
+                         epochs_between_reports: c_uint,
+                         desired_error: c_float) -> Result<(), FannError> {
+        unsafe {
+            fann_sys::fann_train_on_data(self.raw,
+                                         data.raw,
+                                         max_epochs,
+                                         epochs_between_reports,
+                                         desired_error);
+            FannError::check_no_error(self.raw as *mut fann_sys::fann_error)
+        }
+    }
+
+    /// Do the same as `train_on_data` but read the training data directly from a file.
+    pub fn train_on_file<P: AsRef<Path>>(&mut self,
+                                         path: P,
+                                         max_epochs: c_uint,
+                                         epochs_between_reports: c_uint,
+                                         desired_error: c_float) -> Result<(), FannError> {
+        let train = try!(TrainData::from_file(path));
+        self.train_on_data(&train, max_epochs, epochs_between_reports, desired_error)
+    }
+
+    /// Run the input through the neural network and returns the output. The length of the input
+    /// must equal the number of input neurons and the length of the output will equal the number
+    /// of output neurons.
+    pub fn run(&self, input: &[fann_type]) -> Result<Vec<fann_type>, FannError> {
+        try!(self.check_input_size(input));
+        let num_output = self.get_num_output() as usize;
+        let mut result = Vec::with_capacity(num_output);
+        unsafe {
+            let output = fann_sys::fann_run(self.raw, input.as_ptr());
+            try!(FannError::check_no_error(self.raw as *mut fann_sys::fann_error));
+            copy_nonoverlapping(output, result.as_mut_ptr(), num_output);
+            result.set_len(num_output);
+        }
+        Ok(result)
+    }
+
+    /// Get the number of input neurons.
+    pub fn get_num_input(&self) -> c_uint {
+        unsafe { fann_sys::fann_get_num_input(self.raw) }
+    }
+
+    /// Get the number of output neurons.
+    pub fn get_num_output(&self) -> c_uint {
+        unsafe { fann_sys::fann_get_num_output(self.raw) }
+    }
+
+    /// Set the activation function for all hidden layers.
+    pub fn set_activation_func_hidden(&mut self, activation_func: ActivationFunc) {
+        unsafe {
+            let af_enum = activation_func.to_fann_activationfunc_enum();
+            fann_sys::fann_set_activation_function_hidden(self.raw, af_enum);
+        }
+    }
+
+    /// Set the activation function for the output layer.
+    pub fn set_activation_func_output(&mut self, activation_func: ActivationFunc) {
+        unsafe {
+            let af_enum = activation_func.to_fann_activationfunc_enum();
+            fann_sys::fann_set_activation_function_output(self.raw, af_enum);
+        }
+    }
+}
+
+impl Drop for Fann {
+    fn drop(&mut self) {
+        unsafe {
+            fann_sys::fann_destroy(self.raw);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EPSILON: f32 = 0.2;
+
+    #[test]
+    fn test_tutorial() {
+        let max_epochs = 500000;
+        let epochs_between_reports = 1000;
+        let desired_error = 0.001;
+        let mut fann = Fann::new(&[2, 3, 1]).unwrap();
+        fann.set_activation_func_hidden(ActivationFunc::SigmoidSymmetric);
+        fann.set_activation_func_output(ActivationFunc::SigmoidSymmetric);
+        fann.train_on_file("test_files/xor.data",
+                           max_epochs,
+                           epochs_between_reports,
+                           desired_error).unwrap();
+        assert!(EPSILON > ( 1.0 - fann.run(&[-1.0,  1.0]).unwrap()[0]).abs());
+        assert!(EPSILON > ( 1.0 - fann.run(&[ 1.0, -1.0]).unwrap()[0]).abs());
+        assert!(EPSILON > (-1.0 - fann.run(&[ 1.0,  1.0]).unwrap()[0]).abs());
+        assert!(EPSILON > (-1.0 - fann.run(&[-1.0, -1.0]).unwrap()[0]).abs());
+    }
 }
